16bc0a3d4ce91263b469e5e956114dca84ebd5b5
Zhichen Wen <zhichenw@hotmail.com>
2022-08-13 19:14:59 +0800

    separate bachelor & doctoral instances

---
+++ src/components/NewZealand/Education/Doctoral.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="doctoral"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import * as echarts from 'echarts';
+
+onMounted(() => {
+  const doctoralChart = echarts.init(document.getElementById('doctoral') as HTMLElement);
+  doctoralChart.setOption({
+    title: {
+      text: '博士各族裔人数',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'right'
+    },
+    series: [
+      {
+        name: '博士各族裔人数',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 23118, name: '欧裔' },
+          { value: 1197, name: '毛利人' },
+          { value: 348, name: '太平洋诸岛裔' },
+          { value: 4764, name: '亚裔' },
+          { value: 1671, name: '其他' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  })
+})
+</script>
+
+<style scoped>
+#doctoral {
+  width: 700px;
+  height: 500px;
+}
+</style>
--- src/pages/NewZealand/Education.vue
+++ src/pages/NewZealand/Education.vue
@@ -1,14 +1,16 @@
 <template>
   <MainLayout :sidebarCurItem="1">
     <Education />
-    <Ethnicity />
+    <Bachelor />
+    <Doctoral />
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import Education from '@/components/NewZealand/Education/Education.vue';
 import MainLayout from '@/components/Layout/MainLayout.vue';
-import Ethnicity from '@/components/NewZealand/Education/Ethnicity.vue';
+import Bachelor from '@/components/NewZealand/Education/Bachelor.vue';
+import Doctoral from '@/components/NewZealand/Education/Doctoral.vue';
 </script>
 
 <style scoped>
